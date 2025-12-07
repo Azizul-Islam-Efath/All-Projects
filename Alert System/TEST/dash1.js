ef4fab16991f25979bdc5b6862ab684527cae339
Azizul Islam <azizulislam5360@gmail.com>
2025-12-07 18:34:06 +0600

    Change alarm stop time to 10 minutes
    Increased alarm duration from 2 minutes to 10 minutes.

--- Alert System/TEST/dash1.js
+++ Alert System/TEST/dash1.js
@@ -161,7 +161,7 @@ if (workedMins >= ALERT_THRESHOLD_MINS && workedMins < PENALTY_BREACH_MINS && !a
         setTimeout(() => {
             alarm.pause();
             alarm.currentTime = 0;
-        }, 300000); // stop after 2 seconds
+        }, 600000); // stop after 2 seconds
     });
 
     alertTriggered = true;
